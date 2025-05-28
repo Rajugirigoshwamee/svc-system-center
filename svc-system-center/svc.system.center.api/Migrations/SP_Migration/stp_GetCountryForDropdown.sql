a6ebd05574dc3f49ea29c722af31ebbf89287533
RajuGiriGoshwamee <rajugirigoswami09@gmail.com>
2025-05-28 11:33:34 +0530

    code commit

---
+++ svc-system-center/svc.system.center.api/Migrations/SP_Migration/stp_GetCountryForDropdown.sql
@@ -0,0 +1,16 @@
+﻿CREATE OR ALTER PROCEDURE [dbo].[stp_GetCountryForDropdown]
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+    SELECT 
+		C.Id as id,
+		C.Name AS name,
+		C.Code AS code
+	FROM [dbo].[Countries] C WITH(NOLOCK)
+	WHERE 
+		C.IsDeleted=0
+	ORDER BY 
+		C.Name
+
+END
